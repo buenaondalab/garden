fefbde3b3d949ef17cfcf71d1acefd00fd5bd3af
andrea <pierdominici.andrea@gmail.com>
2024-11-03 19:38:55 +0100

    fix runJar script for prod profile

--- runJar.sh
+++ runJar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Package for production and execute
-./mvnw package
+# build, test, package for production and execute
+./mvnw package -Dspring-boot.run.profiles=prod
 java -jar target/garden-0.0.1-SNAPSHOT.jar

161ba40827754386260f1e507111238719b7e8da
andrea <pierdominici.andrea@gmail.com>
2024-11-05 23:55:04 +0100

    add ModelMapper Bean

--- src/main/java/com/buenaondalab/garden/GardenApplication.java
+++ src/main/java/com/buenaondalab/garden/GardenApplication.java
@@ -1,13 +1,20 @@
 package com.buenaondalab.garden;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GardenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GardenApplication.class, args);
+	}
+
+    @Bean
+    ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }

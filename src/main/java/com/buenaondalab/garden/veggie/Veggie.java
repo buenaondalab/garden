23f58136c75d795094217c59e976394455b97838
andrea <pierdominici.andrea@gmail.com>
2024-11-07 16:45:17 +0100

    transform Veggie to a jpa entity

--- src/main/java/com/buenaondalab/garden/veggie/Veggie.java
+++ src/main/java/com/buenaondalab/garden/veggie/Veggie.java
@@ -1,27 +1,25 @@
 package com.buenaondalab.garden.veggie;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@AllArgsConstructor @NoArgsConstructor @Getter @Setter @ToString @EqualsAndHashCode
+@Entity
+@Table(name = "VEGGIE")
+@Data @NoArgsConstructor
 public class Veggie {
-
-    private String name;
-
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
-    private Set<Veggie> associations = new HashSet<>();
 
     public Veggie(String name) {
         this.name = name;
     }
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
     
 }

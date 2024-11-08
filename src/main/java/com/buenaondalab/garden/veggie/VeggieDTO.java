e65445c5f15d6435a1bd503a2b99ce3c52909238
andrea <pierdominici.andrea@gmail.com>
2024-11-08 18:10:03 +0100

    add constructor with name param to VeggieDTO

--- src/main/java/com/buenaondalab/garden/veggie/VeggieDTO.java
+++ src/main/java/com/buenaondalab/garden/veggie/VeggieDTO.java
@@ -5,9 +5,10 @@ import java.util.Set;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data
+@Data @NoArgsConstructor
 public class VeggieDTO {
 
     private Long id;
@@ -17,6 +18,10 @@ public class VeggieDTO {
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     private Set<VeggieDTO> associations = new HashSet<>();
+
+    public VeggieDTO(String name) {
+        this.name = name;
+    }
     
     
 }

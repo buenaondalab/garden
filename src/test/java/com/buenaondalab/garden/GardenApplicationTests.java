51ec3c37f2630ea041cf3001494fb95b133c4c86
andrea <pierdominici.andrea@gmail.com>
2024-11-10 23:20:16 +0100

    add test profile
    (cherry picked from commit a3992bd1d922d0cce80086f269c1182a9c12745a)

--- src/test/java/com/buenaondalab/garden/GardenApplicationTests.java
+++ src/test/java/com/buenaondalab/garden/GardenApplicationTests.java
@@ -2,8 +2,10 @@ package com.buenaondalab.garden;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
 class GardenApplicationTests {
 
 	@Test
